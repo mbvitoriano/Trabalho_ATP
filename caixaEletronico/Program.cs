308830e0eab7bc9675161a25d9b52faa2fd9d5a5
Matheus Vitoriano <54358454+mbvitoriano@users.noreply.github.com>
2019-09-28 15:15:14 -0300

    Update

--- caixaEletronico/Program.cs
+++ caixaEletronico/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace caixaEletronico
 {
@@ -6,12 +6,21 @@ namespace caixaEletronico
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
 
-            // Conta Corrente Comum (CCC) | Conta Corrente Com Cheque Especial (CCE) | Conta Poupança (CP)
-            decimal CCC1, CCC2, CCC3, CCE1, CCE2, CCE3, CP1, CP2, CP3;
-
+            // Conta Corrente Comum (CCC) | Conta Corrente Com Cheque Especial (CCE) | Conta Poupança (CP) | Limites Conta Cheque especial (L1,L2,L3)
+            decimal ccc1, ccc2, ccc3, cce1, cce2, cce3, l1, l2, l3, cp1, cp2, cp3;
+            int opcao = 0;
+            Console.WriteLine("Bem Vindo(a) ao terminal de caixa eletrônico do Banco C#!");
+            Console.WriteLine("Por gentileza, informe a operação desejada:");
+            Console.WriteLine("(1) - Consultar Saldo");
+            Console.WriteLine("(2) - Efetuar um Saque");
+            Console.WriteLine("(3) - Realizar um Depósito");
+            Console.WriteLine("(4) - Efetuar um Pagamento");
+            Console.WriteLine("(5) - Realizar uma Transferência");
+            Console.WriteLine("(6) - Encerrar");
 
         }
     }
 }
+
+
