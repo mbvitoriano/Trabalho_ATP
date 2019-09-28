f4b7530069e076de572e7eb02ec3a753f4f90bfb
Matheus Vitoriano <matheusbzv@gmail.com>
2019-09-28 14:47:13 -0300

    Update

--- caixaEletronico/Program.cs
+++ caixaEletronico/Program.cs
@@ -7,8 +7,11 @@ namespace caixaEletronico
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            //teste
-            //hi
+
+            // Conta Corrente Comum (CCC) | Conta Corrente Com Cheque Especial (CCE) | Conta Poupança (CP)
+            decimal CCC1, CCC2, CCC3, CCE1, CCE2, CCE3, CP1, CP2, CP3;
+
+
         }
     }
 }
